8876a137754b76f68ce3f501c3a1dfe7900380b2
Maxim <m.stepanov@lab244.ru>
2021-12-01 22:43:39 +0300

    edit semver.rb: Minor changes, make process private

--- lib/geode/semver.rb
+++ lib/geode/semver.rb
@@ -4,6 +4,31 @@
 #   - package => 1.0
 module Geode
   class Version < Gem::Version
+    def initialize(ver)
+      super process(ver)
+    end
+
+    def create(ver)
+      super process(ver)
+    end
+
+    def latest(vers, **kws)
+      release = kws.fetch :release, false
+      version = kws.fetch :version, create(@version)
+      last = false
+
+      vers.each do |v|
+        v = create v unless v.is_a? Geode::Version
+        next if version >= v
+        next unless !last || (last < v)
+
+        last = v unless release ? v.prerelease? : false
+      end
+      last
+    end
+
+    private
+
     def process(ver)
       ver = ver.join "." if ver.is_a? Array
       if ver.is_a? Hash
@@ -30,29 +55,6 @@ module Geode
               .join   "."
       end
       ver
-    end
-
-    def initialize(ver)
-      super process ver
-    end
-
-    def create(ver)
-      super process ver
-    end
-
-    def resolve(vers, **kws)
-      release = kws.fetch :release, false
-      version = kws.fetch :version, Geode::Version.create(@version)
-      last = false
-
-      vers.each do |v|
-        v = Geode::Version.create v unless v.is_a? Geode::Version
-        next if version >= v
-        next unless !last || (last < v)
-
-        last = v unless release ? v.prerelease? : false
-      end
-      last
     end
   end
 end
